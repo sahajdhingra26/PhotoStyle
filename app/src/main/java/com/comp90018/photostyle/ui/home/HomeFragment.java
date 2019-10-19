1e79bd55f74f8d07d532188d8a79c600e32ae530
Swapnil Shailee <swapnilshailee27@gmail.com>
2019-10-20 00:23:43 +1100

    Update HomeFragment.java

--- app/src/main/java/com/comp90018/photostyle/ui/home/HomeFragment.java
+++ app/src/main/java/com/comp90018/photostyle/ui/home/HomeFragment.java
@@ -15,6 +15,23 @@ import androidx.lifecycle.ViewModelProviders;
 import com.comp90018.photostyle.R;
 
 public class HomeFragment extends Fragment {
+  //initialise home view model
+  private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
 
 
 }
