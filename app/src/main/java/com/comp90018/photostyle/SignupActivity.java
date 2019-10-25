10feda57b16ca08aee6d974c136374b0b52df140
ArushiSinha <arushisinha92@gmail.com>
2019-10-25 21:13:59 +1100

    Modified Signup for latest version

--- app/src/main/java/com/comp90018/photostyle/SignupActivity.java
+++ app/src/main/java/com/comp90018/photostyle/SignupActivity.java
@@ -10,21 +10,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.comp90018.photostyle.helpers.UserList;
+
+import com.pixplicity.easyprefs.library.Prefs;
+
 import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignupActivity extends AppCompatActivity {
+    APIInterface apiInterface;
     private static final String TAG = "SignupActivity";
 
-    @BindView(R2.id.input_name) EditText _nameText;
-    @BindView(R2.id.input_address) EditText _addressText;
-    @BindView(R2.id.input_email) EditText _emailText;
-    @BindView(R2.id.input_mobile) EditText _mobileText;
-    @BindView(R2.id.input_password) EditText _passwordText;
-    @BindView(R2.id.input_reEnterPassword) EditText _reEnterPasswordText;
-    @BindView(R2.id.btn_signup) Button _signupButton;
-    @BindView(R2.id.link_login) TextView _loginLink;
+    @BindView(R.id.input_name) EditText _nameText;
+
+    @BindView(R.id.input_email) EditText _emailText;
+
+    @BindView(R.id.input_password) EditText _passwordText;
+    @BindView(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
+    @BindView(R.id.btn_signup) Button _signupButton;
+    @BindView(R.id.link_login) TextView _loginLink;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,19 +51,20 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Finish the registration screen and return to the Login activity
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
                 overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+        apiInterface = APIClient.getClient2().create(APIInterface.class);
     }
 
     public void signup() {
         Log.d(TAG, "Signup");
 
         if (!validate()) {
-            onSignupFailed();
+            onSignupFailed("error 1");
             return;
         }
 
@@ -68,34 +77,64 @@ public class SignupActivity extends AppCompatActivity {
         progressDialog.show();
 
         String name = _nameText.getText().toString();
-        String address = _addressText.getText().toString();
+
         String email = _emailText.getText().toString();
-        String mobile = _mobileText.getText().toString();
+
         String password = _passwordText.getText().toString();
-        String reEnterPassword = _reEnterPasswordText.getText().toString();
+
+
 
         // TODO: Implement your own signup logic here.
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        try {
+            Call<UserList> call2 = apiInterface.registerUser(name, email, password);
+            call2.enqueue(new Callback<UserList>() {
+                @Override
+                public void onResponse(Call<UserList> call2, Response<UserList> response) {
+
+                    UserList userList = response.body();
+
+
+                    onSignupSuccess(userList.getEmail());
+                    // onSignupFailed();
+                    progressDialog.dismiss();
+
+
+                }
+
+                @Override
+                public void onFailure(Call<UserList> call, Throwable t) {
+                    call.cancel();
+                    onSignupFailed("error 2");
+                    progressDialog.dismiss();
+
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
     }
 
 
-    public void onSignupSuccess() {
+    public void onSignupSuccess(String email) {
+        Prefs.putString("email", email);
+        Prefs.putBoolean("loggedIn", true);
         _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
+        Toast.makeText(getBaseContext(), "Email has been sent tp your registered email id. Please verify the email and login again.", Toast.LENGTH_LONG).show();
         finish();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
-    public void onSignupFailed() {
+    public void onSignupFailed(String message) {
+        Toast.makeText(getBaseContext(), message, Toast.LENGTH_LONG).show();
+        Prefs.putString("email", "");
+        Prefs.putBoolean("loggedIn", false);
+
+
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
         _signupButton.setEnabled(true);
@@ -105,9 +144,9 @@ public class SignupActivity extends AppCompatActivity {
         boolean valid = true;
 
         String name = _nameText.getText().toString();
-        String address = _addressText.getText().toString();
+
         String email = _emailText.getText().toString();
-        String mobile = _mobileText.getText().toString();
+
         String password = _passwordText.getText().toString();
         String reEnterPassword = _reEnterPasswordText.getText().toString();
 
@@ -118,12 +157,7 @@ public class SignupActivity extends AppCompatActivity {
             _nameText.setError(null);
         }
 
-        if (address.isEmpty()) {
-            _addressText.setError("Enter Valid Address");
-            valid = false;
-        } else {
-            _addressText.setError(null);
-        }
+
 
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
@@ -133,12 +167,7 @@ public class SignupActivity extends AppCompatActivity {
             _emailText.setError(null);
         }
 
-        if (mobile.isEmpty() || mobile.length()!=10) {
-            _mobileText.setError("Enter Valid Mobile Number");
-            valid = false;
-        } else {
-            _mobileText.setError(null);
-        }
+
 
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
             _passwordText.setError("between 4 and 10 alphanumeric characters");
